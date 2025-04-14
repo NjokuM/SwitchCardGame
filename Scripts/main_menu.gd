99f93ad2053388c212de85d17549f07fbb63dfe2
Michael <michaelnjoku1200@gmail.com>
2025-04-14 12:46:13 +0100

    More Scene Changes

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,9 +3,9 @@ extends Control
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/play_menu.tscn")
 	
-func _on_options_pressed() -> void:
-	print("Settings pressed")
+func _on_rules_pressed() -> void:
+	# Load the rules scene
+	get_tree().change_scene_to_file("res://scene/rules_screen.tscn")
 	
 func _on_exit_pressed() -> void:
 	get_tree().quit()
-	
--- Scripts/play_menu.gd
+++ Scripts/play_menu.gd
@@ -4,8 +4,10 @@ func _on_multiplayer_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/multiplayer_menu.tscn")
 
 func _on_local_button_pressed() -> void:
-	pass # Replace with function body.
-
+	get_tree().change_scene_to_file("res://scene/select_player_num_menu.tscn")
 
 func _on_play_cpu_button_pressed() -> void:
-		get_tree().change_scene_to_file("res://scene/select_player_num_menu.tscn")
+	get_tree().change_scene_to_file("res://scene/select_player_num_menu.tscn")
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
